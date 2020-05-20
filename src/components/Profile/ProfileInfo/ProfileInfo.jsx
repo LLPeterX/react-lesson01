d9186ac1b189921fc71a0494686efd571174a4f1
Peter Lebedev <LPeter@mail.ru>
2020-05-20 17:35:34 +0300

    Lesson 84 (useState) done

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,28 +2,20 @@ import React from 'react'
 import s from './ProfileInfo.module.css'
 import defaultAvatar from '../../../assets/images/empty-avatar-png-18.png';
 import Preloader from '../../common/Preloader/Preloader';
-import ProfileStatus from './ProfileStatus';
+//import ProfileStatus from './ProfileStatus';
+import ProfileStatusWithHooks from './ProfileStatusWithHooks';
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
     return <Preloader />
   }
   return (
-    // <div>
-    //   <div className={s.descriptionBlock}>
-    // <div className={s.userName}>ID: {props.profile.userId} {props.profile.fullName}</div>
-    //     <img src={props.profile.photos.large!=null ? props.profile.photos.large : defaultAvatar} alt="Avatar"/>
-    //     <div>
-    //       {props.profile != null ? props.profile.fullName : "Noname"}
-    //     </div>
-    //     </div>
-    // </div>
     <div className={s.container}>
       
       <div className={s.header}>
         <div className={s.header__userInfo}>
           <div className={s.header__userInfo_userName}>{props.profile.fullName}</div>
-          <ProfileStatus status={props.status} updateStatus={props.updateStatus} />
+          <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus} />
         </div>
         <div>
           <img src={props.profile.photos.large != null ? props.profile.photos.large : defaultAvatar} alt="[Avatar]" className={s.avatar} />
---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+let ProfileStatusWithHooks = (props) => {
+
+  // let hookArray = useState(false);  
+  // let editMode = hookArray[0];
+  // let setEditMode = hookArray[1];
+  let [editMode, setEditMode] = useState(false);
+  let [status, setStatus] = useState(props.status);
+
+  const onStatusChange = (event) => {
+    setStatus(event.currentTarget.value);
+  }
+
+  const activateEditMode = () => {
+    setEditMode(true);
+  }
+
+  const deactivateEditMode = () => {
+    setEditMode(false);
+    props.updateStatus(status);
+  }
+
+  return (
+    <div>
+      {!editMode ?
+        <div>  <span onDoubleClick={activateEditMode}>{props.status || '=================='}</span> </div>
+        :
+        <div><input value={status} autoFocus={true} onBlur={deactivateEditMode} onChange={onStatusChange} /></div>
+      }
+    </div>
+  );
+
+
+}
+export default ProfileStatusWithHooks;
