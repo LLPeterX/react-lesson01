0000673d1f99fb9e39cd99817afc27b33e144da4
Peter Lebedev <LPeter@mail.ru>
2020-05-26 21:13:50 +0300

    Updates

---
+++ precache-manifest.fe7b7388c03cba1059678fe4757c9964.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38274aab73051d9428c5fed8194f1671",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d5d5e843e4cb567cc450",
+    "url": "./static/css/4.d54b8a05.chunk.css"
+  },
+  {
+    "revision": "d255a65ecb7f9854cc81",
+    "url": "./static/css/5.72a193e6.chunk.css"
+  },
+  {
+    "revision": "30eaf31283a929abf203",
+    "url": "./static/css/6.236ee19a.chunk.css"
+  },
+  {
+    "revision": "2c5cd3bed8fd6237f177",
+    "url": "./static/css/7.2856c18b.chunk.css"
+  },
+  {
+    "revision": "a3df1ef312fff0152078",
+    "url": "./static/css/main.f03df420.chunk.css"
+  },
+  {
+    "revision": "c029c2f35e75334530c4",
+    "url": "./static/js/0.ce642f87.chunk.js"
+  },
+  {
+    "revision": "b1b43a708bd50616cc16",
+    "url": "./static/js/3.ed15b52d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/3.ed15b52d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5d5e843e4cb567cc450",
+    "url": "./static/js/4.9cab7892.chunk.js"
+  },
+  {
+    "revision": "d255a65ecb7f9854cc81",
+    "url": "./static/js/5.eaeb5420.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "./static/js/5.eaeb5420.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30eaf31283a929abf203",
+    "url": "./static/js/6.b6148e83.chunk.js"
+  },
+  {
+    "revision": "2c5cd3bed8fd6237f177",
+    "url": "./static/js/7.952a5989.chunk.js"
+  },
+  {
+    "revision": "a3df1ef312fff0152078",
+    "url": "./static/js/main.bb1fdd37.chunk.js"
+  },
+  {
+    "revision": "d80347416661ef822c6f",
+    "url": "./static/js/runtime-main.9bd7c17e.js"
+  },
+  {
+    "revision": "5ee8137e5fba4894110a1591572506e0",
+    "url": "./static/media/preloader.5ee8137e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/demoSocNet/precache-manifest.2f12a7ed27a4b8d296984b7fc7220d79.js"
+  "./precache-manifest.fe7b7388c03cba1059678fe4757c9964.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/demoSocNet/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
