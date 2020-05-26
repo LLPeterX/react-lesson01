e524498cb18307da8b57ef6165943c63c1dd7fcd
Peter Lebedev <LPeter@mail.ru>
2020-05-26 18:39:43 +0300

    Updates

---
+++ precache-manifest.938f365a3c925e9a828099e40c1f45a8.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32afe42d05a500091c87330818877b7b",
+    "url": "/demoSocNet/index.html"
+  },
+  {
+    "revision": "d5d5e843e4cb567cc450",
+    "url": "/demoSocNet/static/css/4.d54b8a05.chunk.css"
+  },
+  {
+    "revision": "b1665103aa93d8041d9a",
+    "url": "/demoSocNet/static/css/5.72a193e6.chunk.css"
+  },
+  {
+    "revision": "98b69d19b9319f9d09ea",
+    "url": "/demoSocNet/static/css/6.236ee19a.chunk.css"
+  },
+  {
+    "revision": "2c5cd3bed8fd6237f177",
+    "url": "/demoSocNet/static/css/7.2856c18b.chunk.css"
+  },
+  {
+    "revision": "7ed569707188d127467b",
+    "url": "/demoSocNet/static/css/main.f03df420.chunk.css"
+  },
+  {
+    "revision": "c029c2f35e75334530c4",
+    "url": "/demoSocNet/static/js/0.ce642f87.chunk.js"
+  },
+  {
+    "revision": "f86c68c4062f4614b4f9",
+    "url": "/demoSocNet/static/js/3.61c6574b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demoSocNet/static/js/3.61c6574b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5d5e843e4cb567cc450",
+    "url": "/demoSocNet/static/js/4.9cab7892.chunk.js"
+  },
+  {
+    "revision": "b1665103aa93d8041d9a",
+    "url": "/demoSocNet/static/js/5.c7360625.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/demoSocNet/static/js/5.c7360625.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98b69d19b9319f9d09ea",
+    "url": "/demoSocNet/static/js/6.cab8e6c1.chunk.js"
+  },
+  {
+    "revision": "2c5cd3bed8fd6237f177",
+    "url": "/demoSocNet/static/js/7.952a5989.chunk.js"
+  },
+  {
+    "revision": "7ed569707188d127467b",
+    "url": "/demoSocNet/static/js/main.96e7c352.chunk.js"
+  },
+  {
+    "revision": "d0e20806a5323d48bc64",
+    "url": "/demoSocNet/static/js/runtime-main.7c5e9484.js"
+  },
+  {
+    "revision": "5ee8137e5fba4894110a1591572506e0",
+    "url": "/demoSocNet/static/media/preloader.5ee8137e.svg"
+  }
+]);
