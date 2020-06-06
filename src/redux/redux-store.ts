846f944e5b211e68768ab9d3c5cf82e56edf8c63
Peter Lebedev <LPeter@mail.ru>
2020-06-06 14:41:00 +0300

    Apply TS to redux-store

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,12 +1,12 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
-import profileReducer from './profile-reducer.ts';
-import dialogsReducer from './dialogs-reducer.ts';
-import sidebarReducer from './sidebar-reducer.ts';
-import usersReducer from './users-reducer.ts';
-import authReducer from './auth-reducer.ts';
+import profileReducer from './profile-reducer'
+import dialogsReducer from './dialogs-reducer'
+import sidebarReducer from './sidebar-reducer'
+import usersReducer from './users-reducer'
+import authReducer from './auth-reducer'
 import thunkMiddleware from 'redux-thunk';
 import {reducer as formReducer} from 'redux-form';
-import appReducer from './app-reducer.ts';
+import appReducer from './app-reducer';
 
 // собираем все базовые редюсеры в один объект (в т.ч. объединяем все state)
 let reducers = combineReducers({
@@ -21,6 +21,6 @@ let reducers = combineReducers({
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
 
-window.store = store; // врeменный глобальный объект для отладки
+//window.store = store; // врeменный глобальный объект для отладки
 
 export default store;
